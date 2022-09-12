9f28f32dd5cfc06f80763ed04288a8a6193037ee
qleoz12 <kintaroloop@gmail.com>
2022-09-11 23:10:31 -0500

    modificaciones - readme

---
+++ clean.md
@@ -0,0 +1,11 @@
+# describe for clean over date 
+
+## findings
+
+select RZIMPO,count(RZIMPO) 
+from projecto_datos_abierto.diciembre d 
+group by 1
+order by 1
+
+- A&A LOGISTIC SERVICES S.A.S
+- A&A LOGISTIC SERVICES SAS
--- loaddatatod_db.py
+++ loaddatatod_db.py
@@ -1,4 +1,6 @@
 import csv
+from difflib import get_close_matches
+
 import pandas as pd
 from sqlalchemy import create_engine, types
 
@@ -27,12 +29,45 @@ def read_data_mysql():
     return pd.read_sql('SELECT * FROM diciembre', con=engine)
 
 
+def inexistentes(df):
+
+    n_records = len(df)
+    for columna in df:
+        print("{} | {} | {}".format(
+            df[columna].name, len(df[df[columna].isnull()]) / (1.0 * n_records), df[columna].dtype
+        ))
+
+
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # load_data()
     df=read_data_mysql()
     # print(df.values)
-    print(df.size)
-    print(df.describe())
+    # print(df.size)
+    # print(df.describe())
+
+    # print(list(df))
+
+    # print(df.duplicated('RZIMPO'))
+    df1 = df.groupby(['RZIMPO'])
+    print(df1)
+    # df1['duplicate']=df1.RZIMPO.map(lambda x: get_close_matches(x, df1.RZIMPO, n=2,cutoff=0.8))\
+    #           .apply(pd.Series)
+    # print (df1['duplicate'])
+    print(df.dtypes)
+
+    # print(df1.modelo_unico.value_counts()
+    print(df.shape)
+    # print(inexistentes(df1))
+    #fecha
+    print(df['PAISCOM'])
+
+    df['pedidos_unicos'] = df.FECH.astype(str).str.cat([df.PAISGEN.astype(str), df.PAISPRO.apply(str)], sep='-')
+    print(df)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
