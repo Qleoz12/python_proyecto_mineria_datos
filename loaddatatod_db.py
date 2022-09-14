4837fa8c97baebee6665f062cbe172d63613eca1
qleoz12 <kintaroloop@gmail.com>
2022-09-14 11:09:15 -0500

    modificaciones mapeo de nombres para crear diccionario

--- loaddatatod_db.py
+++ loaddatatod_db.py
@@ -49,8 +49,70 @@ if __name__ == '__main__':
     # print(df.describe())
 
     # print(list(df))
+    # df.rename(columns={
+    #     "DEPIM": "Departamento_del_importador",
+    #     "NABAM": "Posición_arancelaria",
+    #     "CANU": "Cantidad_de_unidades",
+    #     "CODA": "Codigo_de_unidad",
+    #     "PNK": "Peso_neto_en_kilos",
+    #     "FECH": "Fecha_de_proceso",
+    #     "VADUA": "Valor_Aduana",
+    #     "TOTALIVAYO": "Total_IVA_y_otros_gastos",
+    #     "PAISCOM": "Pais_de_compra",
+    #     "PAISPRO": "Pais_productor",
+    #     "PAISGEN": "Pais_origen",
+    #     "VACIP": "Valor_en_pesos",
+    #     "VAFODO": "Valor_en_dolares",
+    #     "RZIMPO": "Valor_en_Razon_social_del_importador",
+    #     "CLASE": "Clase_de_importador",
+    # })
 
+    # ADUA      Código de la aduana
+    # PAISGEN   País origen
+    # PAISPRO   País de procedencia
+    # PAISCOM   País de compra
+    # DEPTODES  Departamento destino
+    # VIATRANS  Código vía de transporte
+    # BANDERA
+    # REGIMEN
+    # ACUERDO
+    # PBK       Peso bruto en kilos
+    # PNK
+    # CANU      Cantidad de unidades
+    # CODA      Código de unidad
+    # NABAN     Posición arancelaria
+    # VAFODO    Valor FOB dólares de la mercancía
+    # FLETE     Fletes
+    # VACID     Valor CIF dólares de la mercancía
+    # VACIP     Valor CIF pesos de la mercancía
+    # IMP1      Impuesto a las ventas
+    # OTDER     Otros Derechos
+    # CLASE     Clase de importador
+    # CUIDAIMP  Ciudad del importador
+    # CUIDAEXP  Ciudad del exportador
+    # ACTECON   Actividad económica
+    # CODADAD   Código administración de aduana
+    # VADUA     Valor aduana
+    # VRAJUS    Valor ajuste
+    # BASEIVA
+    # OTROSP     Porcentaje otros
+    # OTROSBASE
+    # TOTALIVAYO  Total IVA y otros gastos
+    # SEGUROS    Seguros
+    # OTROSG     Otros gastos
+    # LUIN       Lugar de ingreso
+    # CODLUIN    Código lugar de ingreso
+    # DEPIM      Departamento del importador
+    # COPAEX     Código país del exportador
+    # TIPOIM     Tipo de importación
+    # PORARA     Porcentaje de arancel
+    # NIT
+    # DIGV       Digito de Verificación
+    # RZIMPO
+    # DEREL      Derechos Arancelarios
     # print(df.duplicated('RZIMPO'))
+
+
     df1 = df.groupby(['RZIMPO'])
     print(df1)
     # df1['duplicate']=df1.RZIMPO.map(lambda x: get_close_matches(x, df1.RZIMPO, n=2,cutoff=0.8))\
